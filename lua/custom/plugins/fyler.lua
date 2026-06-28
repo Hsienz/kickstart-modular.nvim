6c7e379088f0f4304ffd9fa561da76090fe294db
yumunull <yumunull@gmail.com>
2026-06-29 01:33:42 +0800

    feat: update

--- lua/custom/plugins/fyler.lua
+++ lua/custom/plugins/fyler.lua
@@ -2,55 +2,56 @@ return {
   'A7Lavinraj/fyler.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   lazy = true,
-  commit = 'e87911e6c21d099225063f5aa672e00f6dbb5976',
   keys = {
-    { '<leader>e', '<Cmd>Fyler kind=split_left_most<Cr>', desc = 'Open Fyler View' },
+    { '<leader>e', '<Cmd>Fyler<Cr>', desc = 'Fyler.nvim - Open' },
   },
   opts = {
+    use_as_default_explorer = false,
     hooks = {
       on_rename = function(src_path, destination_path) Snacks.rename.on_rename_file(src_path, destination_path) end,
     },
     integrations = {
       icon = 'nvim_web_devicons',
+      window_picker = function() return require('snacks').picker.util.pick_win() end,
     },
-    views = {
-      finder = {
-        default_explorer = false,
-        columns_order = { 'git', 'diagnostic' },
-        columns = {
-          git = {
-            enabled = true,
-            symbols = {
-              Untracked = '',
-              Added = '',
-              Modified = '',
-              Deleted = '',
-              Renamed = '',
-              Copied = '󱐪',
-              Conflict = '󰦦',
-              Ignored = '',
-            },
-          },
-          diagnostic = {
-            enabled = true,
-            symbols = {
-              Error = '󰅜',
-              Warn = '󱇎',
-              Info = '',
-              Hint = '',
-            },
-          },
-          link = {
-            enabled = false,
-          },
-          permission = {
-            enabled = false,
-          },
-          size = {
-            enabled = false,
-          },
+
+    kind = 'split_left_most',
+    kind_presets = {
+      split_left_most = {
+        mappings = { n = { ['<CR>'] = { action = 'select', args = { close = true, pick = false } } } },
+      },
+    },
+    extensions = {
+      git = {
+        enabled = true,
+        inline = true,
+        icons = {
+          [' M'] = { icon = '', hl = 'FylerGitModified' },
+          ['M '] = { icon = '', hl = 'FylerGitStaged' },
+          ['MM'] = { icon = '', hl = 'FylerGitStaged' },
+          ['??'] = { icon = '', hl = 'FylerGitUntracked' },
+          [' D'] = { icon = '', hl = 'FylerGitDeleted' },
+          ['D '] = { icon = '', hl = 'FylerGitStaged' },
+          ['R '] = { icon = '', hl = 'FylerGitRenamed' },
+          ['UU'] = { icon = '󰦦', hl = 'FylerGitConflict' },
+          ['!!'] = { icon = '', hl = 'FylerGitIgnored' },
         },
       },
+      -- diagnostic = {
+      --   enabled = true,
+      --   symbols = {
+      --     Error = '󰅜',
+      --     Warn = '󱇎',
+      --     Info = '',
+      --     Hint = '',
+      --   },
+      -- },
+    },
+    ui = {
+      hidden_items = {
+        patterns = { '%.asset$', '%.prefab$', '%.meta$' },
+      },
+      indent_guides = true,
     },
   },
 }
