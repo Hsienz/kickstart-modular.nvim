a6286dc2072c0503a183f2252597b882ccf4d651
leoydt <leo.hsu@yudutek.cc>
2026-07-20 15:47:22 +0800

    feat: pin fyler

--- lua/custom/plugins/fyler.lua
+++ lua/custom/plugins/fyler.lua
@@ -1,54 +1,50 @@
 local function gh(repo) return 'https://github.com/' .. repo end
-
-vim.pack.add { { src = gh 'A7Lavinraj/fyler.nvim' }, { src = gh 'nvim-tree/nvim-web-devicons' } }
+vim.pack.add { { src = gh 'A7Lavinraj/fyler.nvim', version = 'e87911e6c21d099225063f5aa672e00f6dbb5976' }, { src = gh 'nvim-tree/nvim-web-devicons' } }
+vim.keymap.set('n', '<leader>e', '<Cmd>Fyler kind=split_left_most<Cr>', { desc = 'Open Fyler View' })
 require('fyler').setup {
-  use_as_default_explorer = false,
   hooks = {
     on_rename = function(src_path, destination_path) Snacks.rename.on_rename_file(src_path, destination_path) end,
   },
   integrations = {
     icon = 'nvim_web_devicons',
-    window_picker = function() return require('snacks').picker.util.pick_win() end,
   },
-
-  kind = 'split_left_most',
-  kind_presets = {
-    split_left_most = {
-      mappings = { n = { ['<CR>'] = { action = 'select', args = { close = true, pick = true } } } },
-    },
-  },
-  extensions = {
-    git = {
-      enabled = true,
-      inline = false,
-      icons = {
-        [' M'] = { icon = ' ', hl = 'FylerGitModified' },
-        ['M '] = { icon = ' ', hl = 'FylerGitStaged' },
-        ['MM'] = { icon = ' ', hl = 'FylerGitStaged' },
-        ['??'] = { icon = ' ', hl = 'FylerGitUntracked' },
-        [' D'] = { icon = ' ', hl = 'FylerGitDeleted' },
-        ['D '] = { icon = ' ', hl = 'FylerGitStaged' },
-        ['R '] = { icon = ' ', hl = 'FylerGitRenamed' },
-        ['UU'] = { icon = '󰦦 ', hl = 'FylerGitConflict' },
-        ['!!'] = { icon = ' ', hl = 'FylerGitIgnored' },
+  views = {
+    finder = {
+      default_explorer = false,
+      columns_order = { 'git', 'diagnostic' },
+      columns = {
+        git = {
+          enabled = true,
+          symbols = {
+            Untracked = '',
+            Added = '',
+            Modified = '',
+            Deleted = '',
+            Renamed = '',
+            Copied = '󱐪',
+            Conflict = '󰦦',
+            Ignored = '',
+          },
+        },
+        diagnostic = {
+          enabled = true,
+          symbols = {
+            Error = '󰅜',
+            Warn = '󱇎',
+            Info = '',
+            Hint = '',
+          },
+        },
+        link = {
+          enabled = false,
+        },
+        permission = {
+          enabled = false,
+        },
+        size = {
+          enabled = false,
+        },
       },
     },
-    -- diagnostic = {
-    --   enabled = true,
-    --   symbols = {
-    --     Error = '󰅜',
-    --     Warn = '󱇎',
-    --     Info = '',
-    --     Hint = '',
-    --   },
-    -- },
-  },
-  ui = {
-    hidden_items = {
-      patterns = { '%.asset$', '%.prefab$', '%.meta$' },
-    },
-    indent_guides = true,
   },
 }
-
-vim.keymap.set('n', '<leader>e', '<Cmd>Fyler<Cr>', { desc = 'Fyler.nvim - Open' })
