fa7d574c7a5ab26859b8e4b073a4382a8e1a1864
leoydt <leo.hsu@yudutek.cc>
2026-08-04 18:07:23 +0800

    feat: update

--- lua/custom/plugins/clangd_extensions.lua
+++ lua/custom/plugins/clangd_extensions.lua
@@ -1,3 +1,26 @@
 local function gh(repo) return 'https://github.com/' .. repo end
 
 vim.pack.add { { src = gh 'dchinmay2/clangd_extensions.nvim' } }
+
+require('clangd_extensions').setup {
+  ast = {
+    role_icons = {
+      type = '',
+      declaration = '',
+      expression = '',
+      specifier = '',
+      statement = '',
+      ['template argument'] = '',
+    },
+
+    kind_icons = {
+      Compound = '',
+      Recovery = '',
+      TranslationUnit = '',
+      PackExpansion = '',
+      TemplateTypeParm = '',
+      TemplateTemplateParm = '',
+      TemplateParamObject = '',
+    },
+  },
+}
